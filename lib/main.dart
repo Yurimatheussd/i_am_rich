4a462e9ea1e273554aaac61f003bd3949e98c311
Yurimatheussd <yuri.dias@linx.com.br>
2026-02-12 03:10:36 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+// A função main é o ponto de inicio para todos apps Flutter
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor:
+            Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'I Am Rich',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor:
+              Colors.blueGrey[900],
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: Image(
+                image: NetworkImage(
+                  'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                ),
+              ),
+            ),
+            Image.asset(
+              'assets/teste.jpg',
+            ),
+            Image.asset('assets/teste.jpg'),
+          ],
+        ),
+      ),
+    ),
+  );
+}
